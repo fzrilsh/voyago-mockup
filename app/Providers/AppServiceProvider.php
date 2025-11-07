a626608bd1a60ab7e8521e586c7607e69e2aa3f9
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-07 20:39:52 +0700

    Add environment configuration and Livewire settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force asset URL for subfolder deployment
+        if ($assetUrl = env('ASSET_URL')) {
+            URL::forceRootUrl($assetUrl);
+        }
     }
 }
